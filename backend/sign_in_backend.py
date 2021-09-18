4c79e296711a3c2342cd98b8d0d98b11425daf93
DasAnish <ad945@cam.ac.uk>
2021-09-18 17:59:51 +0100

    sign in/up verification backend complete

--- backend/sign_in_backend.py
+++ backend/sign_in_backend.py
@@ -1,3 +1,6 @@
+from backend import Mongo
+from backend import Parent, Tutor
+
 
 class SignInBackend:
 
@@ -17,7 +20,53 @@ class SignInBackend:
         else:
             SignInBackend.__instance = self
 
-    def verify(self, loginInfo):
-        raise Exception("not implemented")
+    def signUpVerification(self, loginInfo):
+        username = loginInfo['username']
+        isTutor = loginInfo['isTutor']
+
+        mongo = Mongo.getInstance()
+
+        if isTutor:
+            collection = mongo.tutorsData
+            creator = Tutor
+        else:
+            collection = mongo.parentsData
+            creator = Parent
+
+        # checking that the username does not exists
+        query = {"username": username}
+        output = collection.find(query)
+
+        if output.count_documents() != 0:
+            return False
+
+        del loginInfo['isTutor']
+
+        _id = collection.insert_one(loginInfo)
+
+        obj = creator(_id)
+        obj.updateInfo(loginInfo)
+
+        return True
+
+    def signInVerification(self, loginInfo):
+        username = loginInfo['username']
+        password = loginInfo['password']
+        isTutor = loginInfo['isTutor']
+
+        mongo = Mongo.getInstance()
+
+        query = {"username": username}
+        if isTutor:
+            output = mongo.tutorsData.find(query)
+        else:
+            output = mongo.parentsData.find(query)
+
+        if not output.count_documents():
+            return False
+
+        output = output[0]
+
+        return output['password'] == password
 
 
